3e3e18b26cf6ed7c9847c64efaeffdd582105a26
AlexisDerumigny <aderumigny@gmail.com>
2024-02-08 14:38:39 +0100

    Stricter check on `PCBN` objects + automatic column names

---
+++ tests/testthat/test-PCBN-object.R
@@ -0,0 +1,53 @@
+test_that("new_PCBN respect contraints on copula_mat", {
+  DAG = create_DAG(3)
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U3')
+  DAG = bnlearn::set.arc(DAG, 'U2', 'U3')
+
+  order_hash = r2r::hashmap()
+  order_hash[['U3']] = c("U1", "U2")
+
+  expect_error(new_PCBN(DAG = DAG, order_hash = order_hash,
+                        copula_mat = NULL))
+
+  expect_error(new_PCBN(DAG = DAG, order_hash = order_hash,
+                        copula_mat = list() ) )
+
+  expect_error(new_PCBN(DAG = DAG, order_hash = order_hash,
+                        copula_mat = list(tau = 0.5, fam = 1) ) )
+
+  expect_error(new_PCBN(DAG = DAG, order_hash = order_hash,
+                        copula_mat = list(tau = matrix(1:9, ncol = 9),
+                                          fam = matrix(1:9, ncol = 9)) ) )
+})
+
+
+test_that("new_PCBN controls names on copula_mat", {
+  DAG = create_DAG(3)
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U3')
+  DAG = bnlearn::set.arc(DAG, 'U2', 'U3')
+
+  order_hash = r2r::hashmap()
+  order_hash[['U3']] = c("U1", "U2")
+
+  fam = matrix(c(0, 1, 1,
+                 0, 0, 1,
+                 0, 0, 0), byrow = TRUE, ncol = 3)
+  tau = 0.2 * fam
+
+  my_PCBN = new_PCBN(
+    DAG, order_hash,
+    copula_mat = list(tau = tau, fam = fam))
+
+  rownames(fam) <- c("U1", "U2", "U3")
+  colnames(fam) <- c("U1", "U2", "U3")
+
+  my_PCBN = new_PCBN(
+    DAG, order_hash,
+    copula_mat = list(tau = tau, fam = fam))
+
+  rownames(fam) <- c("U1", "U2", "aaaaa")
+
+  expect_error(new_PCBN(
+    DAG, order_hash,
+    copula_mat = list(tau = tau, fam = fam)))
+})
