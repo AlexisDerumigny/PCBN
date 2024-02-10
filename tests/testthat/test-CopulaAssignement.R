AlexisDerumigny/PCBN

commit 019395fe87dd4239895753f05e9b231b19285cda

Fix bug in `is_cond_copula_specified` that appeared for nodes with 1 parent

R/CopulaAssignement.R
@@ -225,7 +225,7 @@ is_cond_copula_specified <- function(DAG, order_hash, w, v, cond){
   if (dsep_set(DAG, w, v, cond)){
     return(TRUE)
   }
-  parents_v = order_hash[[v]]
+  parents_v = bnlearn::parents(x = DAG, node = v)
   if (w %in% parents_v){
     index_w_in_parents = which(parents_v == w)
     parents_up_to_w = if(index_w_in_parents == 1) { c() } else {
@@ -235,7 +235,7 @@ is_cond_copula_specified <- function(DAG, order_hash, w, v, cond){
       return(TRUE)
     }
   }
-  parents_w = order_hash[[w]]
+  parents_w = bnlearn::parents(x = DAG, node = w)
   if (v %in% parents_w){
     index_v_in_parents = which(parents_w == v)
     parents_up_to_v = if(index_v_in_parents == 1) { c() } else {

tests/testthat/test-CopulaAssignement.R
@@ -1,8 +1,9 @@
-test_that("is_cond_copula_specified works for 3 dimensional example", {
+test_that("is_cond_copula_specified works for 4 dimensional example", {
 
-  DAG = create_DAG(3)
+  DAG = create_DAG(4)
   DAG = bnlearn::set.arc(DAG, 'U1', 'U3')
   DAG = bnlearn::set.arc(DAG, 'U2', 'U3')
+  DAG = bnlearn::set.arc(DAG, 'U3', 'U4')
 
   order_hash = r2r::hashmap()
   order_hash[['U3']] = c("U1", "U2")
@@ -24,10 +25,16 @@ test_that("is_cond_copula_specified works for 3 dimensional example", {
   expect_true(is_cond_copula_specified(DAG = DAG, order_hash = order_hash,
                                        w = "U3", v = "U2", cond = c("U1")))
   # returns TRUE because the copula c_{2,3 | 1} is known
+
+  expect_true(is_cond_copula_specified(DAG = DAG, order_hash = order_hash,
+                                       w = "U3", v = "U4", cond = c()) )
+  expect_true(is_cond_copula_specified(DAG = DAG, order_hash = order_hash,
+                                       w = "U4", v = "U3", cond = c()) )
+  # returns TRUE because the copula c_{3,4} is known
 })
 
 
-test_that("find_cond_copula_specified works for 1 dimensional example", {
+test_that("find_cond_copula_specified works for 4 dimensional example", {
 
 
   DAG = create_DAG(4)

commit 8ba4a2e965b413965daff2625d79249f614253d9

Fix bug in `is_cond_copula_specified` when a node has several parents in the wrong order
bug introduced by 019395fe87dd4239895753f05e9b231b19285cda , which fixed another similar bug but slightly different: the case where only parent is present.

R/CopulaAssignement.R
@@ -225,7 +225,12 @@ is_cond_copula_specified <- function(DAG, order_hash, w, v, cond){
   if (dsep_set(DAG, w, v, cond)){
     return(TRUE)
   }
-  parents_v = bnlearn::parents(x = DAG, node = v)
+
+  # If there is a value in 'order_hash[[v]]', we should use this
+  # as the order. But if it is null, 'v' may still have at most
+  # one parent.
+  parents_v = if(!is.null(order_hash[[v]])) {
+    order_hash[[v]]} else {bnlearn::parents(x = DAG, node = v)}
   if (w %in% parents_v){
     index_w_in_parents = which(parents_v == w)
     parents_up_to_w = if(index_w_in_parents == 1) { c() } else {
@@ -235,7 +240,12 @@ is_cond_copula_specified <- function(DAG, order_hash, w, v, cond){
       return(TRUE)
     }
   }
-  parents_w = bnlearn::parents(x = DAG, node = w)
+
+  # If there is a value in 'order_hash[[w]]', we should use this
+  # as the order. But if it is null, 'w' may still have at most
+  # one parent.
+  parents_w = if(!is.null(order_hash[[w]])) {
+    order_hash[[w]]} else {bnlearn::parents(x = DAG, node = w)}
   if (v %in% parents_w){
     index_v_in_parents = which(parents_w == v)
     parents_up_to_v = if(index_v_in_parents == 1) { c() } else {

tests/testthat/test-CopulaAssignement.R
@@ -34,6 +34,28 @@ test_that("is_cond_copula_specified works for 4 dimensional example", {
 })
 
 
+test_that("is_cond_copula_specified works for another 4 dimensional example", {
+
+  DAG = create_DAG(4)
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U2')
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U3')
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U4')
+  DAG = bnlearn::set.arc(DAG, 'U2', 'U4')
+  DAG = bnlearn::set.arc(DAG, 'U3', 'U4')
+
+  order_hash = r2r::hashmap()
+  order_hash[['U4']] = c("U2", "U1", "U3")
+
+  expect_true(is_cond_copula_specified(DAG = DAG, order_hash = order_hash,
+                                       w = "U2", v = "U4",
+                                       cond = NULL))
+
+  expect_true(is_cond_copula_specified(DAG = DAG, order_hash = order_hash,
+                                       w = "U4", v = "U2",
+                                       cond = NULL))
+})
+
+
 test_that("find_cond_copula_specified works for 4 dimensional example", {
 
 