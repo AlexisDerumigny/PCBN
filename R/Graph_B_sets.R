03833d2c505a7a76844071dd70dba1d2e30c3145
AlexisDerumigny <aderumigny@gmail.com>
2024-02-28 13:22:54 +0100

    Improve examples of `B_sets_are_increasing`

--- R/Graph_B_sets.R
+++ R/Graph_B_sets.R
@@ -132,15 +132,15 @@ find_B_sets_v <- function(DAG, v)
 #' B_sets = matrix(c(FALSE, FALSE, FALSE, FALSE,
 #'                   TRUE , FALSE, FALSE, FALSE,
 #'                   TRUE , TRUE , FALSE, FALSE,
-#'                   TRUE , TRUE ,  TRUE,  TRUE),
+#'                   TRUE , TRUE , TRUE ,  TRUE),
 #'                 nrow = 4, byrow = TRUE)
 #'
 #' B_sets_are_increasing(B_sets)
 #'
 #' B_sets = matrix(c(FALSE, FALSE, FALSE, FALSE,
-#'                   TRUE , FALSE, TRUE, FALSE,
+#'                   TRUE , FALSE, TRUE , FALSE,
 #'                   TRUE , TRUE , FALSE, FALSE,
-#'                   TRUE , TRUE ,  TRUE,  TRUE),
+#'                   TRUE , TRUE , TRUE ,  TRUE),
 #'                 nrow = 4, byrow = TRUE)
 #'
 #' B_sets_are_increasing(B_sets)
