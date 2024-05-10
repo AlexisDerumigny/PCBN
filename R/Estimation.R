7c4619bffbc9aab665d1165fa585f8905c2d695c
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2024-05-10 15:31:18 +0200

    Fix vignette `Internal-estimation` to pass the stricter checks in 5815cb695ec78006981cfc0b34a6cf3d6b7251c1
    + fix example of estimation for the same reason

--- R/Estimation.R
+++ R/Estimation.R
@@ -46,7 +46,7 @@ default_envir <- function(){
 #' order_hash = r2r::hashmap()
 #' order_hash[['U3']] = c("U1", "U2")
 #'
-#' fam = matrix(c(0, 1, 1,
+#' fam = matrix(c(0, 0, 1,
 #'                0, 0, 1,
 #'                0, 0, 0), byrow = TRUE, ncol = 3)
 #'
