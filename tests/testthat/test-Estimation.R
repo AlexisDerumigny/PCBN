a8d2e60be49b4d8d625e02f55dece5842fa7762d
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2024-05-10 16:29:19 +0200

    Add more unit tests for the estimation

--- tests/testthat/test-Estimation.R
+++ tests/testthat/test-Estimation.R
@@ -126,8 +126,11 @@ test_that("BiCopCondFit completes the hashmaps as needed",  {
                e = e, verbose = verbose)
 
   # Both copulas are different, so the keys should also be different
-  expect_true(! identical(e$keychain[[list(margins = c("U1", "U2"), cond = character(0))]] ,
-                          e$keychain[[list(margins = c("U1", "U3"), cond = character(0))]] ) )
+  expect_true(! identical(e$keychain[[list(margins = c("U1", "U2"),
+                                           cond = character(0))]] ,
+
+                          e$keychain[[list(margins = c("U1", "U3"),
+                                           cond = character(0))]] ) )
 
   expect_equal(e$margin_hash[[ e$keychain[[ list(margin = c("U3"),
                                                  cond = character(0)) ]] ]] |>
@@ -175,6 +178,50 @@ test_that("ComputeCondMargin works", {
 })
 
 
+test_that("fit_copulas gives the same results as what is in the keychain", {
+  e = default_envir()
+
+  result = fit_copulas(data = mydata, DAG = DAG, order_hash = order_hash,
+                       familyset = 1, e = e, verbose = verbose)
+
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U1", "U2"), cond = character(0))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U1", "U2"]
+  )
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U1", "U3"), cond = character(0))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U1", "U3"]
+  )
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U2", "U4"), cond = character(0))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U2", "U4"]
+  )
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U1", "U4"), cond = c("U2"))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U1", "U4"]
+  )
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U3", "U4"), cond = c("U1", "U2"))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U3", "U4"]
+  )
+})
+
+
 test_that("fit_copulas respects the adjacency matrix", {
 
   DAG = create_DAG(3)
@@ -196,12 +243,69 @@ test_that("fit_copulas respects the adjacency matrix", {
   mydata = PCBN_sim(my_PCBN, N = 5)
   e = default_envir()
 
-  result = fit_copulas(data = mydata, DAG = DAG,
-                       order_hash = order_hash, familyset = 1, e = e, verbose = 0)
+  result = fit_copulas(data = mydata, DAG = DAG, order_hash = order_hash,
+                       familyset = 1, e = e, verbose = verbose)
 
 
   expect_equal(result$copula_mat$fam, my_PCBN$copula_mat$fam)
 })
+
+
+test_that("fit_copulas works for an example of dimension 5", {
+
+  # Initialize PCBN
+  DAG = create_DAG(3)
+  DAG = bnlearn::set.arc(DAG, 'U1', 'U3')
+  DAG = bnlearn::set.arc(DAG, 'U2', 'U3')
+
+  order_hash = r2r::hashmap()
+  order_hash[['U3']] = c("U1", "U2")
+  complete_and_check_orders(DAG, order_hash)
+
+  fam = matrix(c(0, 0, 1,
+                 0, 0, 1,
+                 0, 0, 0), byrow = TRUE, ncol = 3)
+  tau = fam * 0.8
+
+  my_PCBN = new_PCBN(
+    DAG, order_hash,
+    copula_mat = list(tau = tau, fam = fam))
+
+  N = 5000
+  mydata = PCBN_sim(object = my_PCBN, N = N)
+
+  parent_given_lower = compute_sample_margin(object = my_PCBN,
+                                             data = mydata,
+                                             v = "U2",
+                                             cond_set = "U1",
+                                             check_PCBN = FALSE)
+
+  par_ = VineCopula::BiCopTau2Par(family = 1, tau = 0.8)
+  U3_correct =
+    VineCopula::BiCopHinv1(u1 = mydata[, "U1"],
+                           u2 =  VineCopula::BiCopHinv1(u1 = mydata[, "U2"],
+                                                        u2 = runif(N),
+                                                        family = 1,
+                                                        par = par_),
+                           family = 1,
+                           par = par_)
+
+  plot(mydata[, "U1"], U3_correct)
+  cor(mydata[, "U1"], U3_correct, method = "kendall")
+  plot(mydata[, "U1"], mydata[, "U3"])
+
+
+  e = default_envir()
+
+  result = fit_copulas(data = mydata, DAG = DAG,
+                       order_hash = order_hash, familyset = 1,
+                       e = e, verbose = verbose)
+
+  expect_equal(object = result$copula_mat$tau |> unname(),
+               expected = tau, tolerance = 0.05)
+
+})
+
 
 
 test_that("fit_copulas works for an example of dimension 5", {
@@ -233,15 +337,35 @@ test_that("fit_copulas works for an example of dimension 5", {
     copula_mat = list(tau = tau, fam = fam))
 
   N = 5000
-  PCBN_sim_data = PCBN_sim(object = my_PCBN, N = N)
+  mydata = PCBN_sim(object = my_PCBN, N = N)
+
+  e = default_envir()
 
   expect_no_error({
-    result = fit_copulas(data = PCBN_sim_data, DAG = DAG,
+    result = fit_copulas(data = mydata, DAG = DAG,
                          order_hash = order_hash, familyset = 1,
-                         e = default_envir(), verbose = 0)
+                         e = e, verbose = verbose)
   })
 
   result$copula_mat$tau
+
+  expect_equal(
+    e$copula_hash[[
+      e$keychain[[list(margins = c("U1", "U3"), cond = character(0))]]
+    ]]$tau ,
+
+    result$copula_mat$tau["U1", "U3"]
+  )
+
+  expect_equal(
+    BiCopCondFit(data = mydata, DAG = DAG, v = "U1", w = "U3",
+                 cond_set = c(), familyset = 1, order_hash = order_hash,
+                 e = e, verbose = verbose)$tau ,
+
+    BiCopCondFit(data = mydata, DAG = DAG, v = "U1", w = "U3",
+                 cond_set = c(), familyset = 1, order_hash = order_hash,
+                 e = default_envir(), verbose = verbose)$tau
+  )
 })
 
 
